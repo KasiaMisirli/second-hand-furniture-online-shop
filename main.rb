e27a71ee05040c8e9a3a0bf9dac55f2e251b2f43
kasia-misirli <kasia.misirli@gmail.com>
2018-04-18 14:21:08 +1000

    adding more files

--- main.rb
+++ main.rb
@@ -2,11 +2,16 @@
 require 'sinatra'
 require 'active_record'
 require_relative 'db_config'
+require 'sinatra/reloader'
+require 'pg'
+require_relative 'models/user'
+require_relative 'models/item'
+require_relative 'models/image'
 
 get '/' do
+  @items = Item.all
   erb :index
 end
-
 
 
 
---
+++ secondhandfurniture.sql
@@ -0,0 +1,24 @@
+-- CREATE DATABASE secondhandfurniture; 
+
+-- CREATE TABLE users (
+--     id SERIAL4 PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     email VARCHAR(100) NOT NULL,
+--     tel VARCHAR(100)
+-- );
+
+-- CREATE TABLE items (
+--     id SERIAL4 PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     user_id INTEGER NOT NULL,
+--     description VARCHAR(400),
+--     price VARCHAR(100),
+--     pricetype VARCHAR(100),
+--     location VARCHAR(400)
+-- );
+
+-- CREATE TABLE images (
+--     id SERIAL4 PRIMARY KEY,
+--     image VARCHAR(100),
+--     item_id INTEGER NOT NULL
+-- );
